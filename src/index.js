f9d0014a8c8d7530b03ff0f890026c7c21e29f75
Nick <nresheto18@gmail.com>
2023-12-24 23:41:52 +0400

    add files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss'
+import cloudy from './assets/cloudly.png'
+
+const mainImage = document.getElementById('mainImage')
+mainImage.src = cloudy
+
+const hello = require('./js/hello')
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        filename: path.resolve(__dirname, 'src/index.js')
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        assetModuleFilename: '[name][ext]',
+        clean: true
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 512000,
+        maxEntrypointSize: 512000,
+    },
+    devServer: {
+        port: 9000,
+        compress: true,
+        hot: true,
+        static: {
+            directory: path.join(__dirname, 'dist')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My web page',
+            name: 'index.html',
+            template: 'src/index.html'
+        })
+    ]
+}
